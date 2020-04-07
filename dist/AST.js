3b6e3a9f3ae3abcd613c30a908a79597cfc117b5
clovis <clovis@byte49.exposed>
2020-04-07 14:58:48 +0200

    Finalisation de l'extracteur

--- dist/AST.js
+++ dist/AST.js
@@ -10,13 +10,85 @@ var AST = /** @class */ (function () {
         this.constructr = null;
         this.didMount = null;
         this.willUnmount = null;
+        this.helpers = [];
+        this.events = [];
+        this.props = [];
+        this.state = [];
         this.tree = Parser.parse(code, {
             sourceType: 'module',
         });
         this.processImports();
         this.processFunctions();
         this.processLifeCycle();
+        this.processHelpers();
+        this.processEvents();
+        this.processProps();
+        this.processState();
     }
+    AST.prototype.getNodeName = function () {
+        AST.NODE_COUNTER++;
+        return AST.NODE_COUNTER.toString();
+    };
+    AST.prototype.processState = function () {
+        var _this = this;
+        traverse_1.default(this.tree, {
+            AssignmentExpression: function (p) {
+                var path = p;
+                var expr = path.node;
+                if (Babel.isMemberExpression(expr.left) == false)
+                    return;
+                var subject = expr.left;
+                // Template.instance(), templateInstance ou this
+                if ((Babel.isThisExpression(subject.object)
+                    || (Babel.isIdentifier(subject.object) && subject.object.name === 'templateInstance')
+                    || (Babel.isCallExpression(subject.object)
+                        && subject.object.callee.object.name === 'Template'
+                        && subject.object.callee.property.name === 'instance')) == false)
+                    return;
+                // = new ReactiveTruc
+                if ((Babel.isNewExpression(expr.right)
+                    && Babel.isIdentifier(expr.right.callee)
+                    && (expr.right.callee.name === "ReactiveVar"
+                        || expr.right.callee.name === "ReactiveDict")) == false)
+                    return;
+                // nom du state
+                if (Babel.isIdentifier(subject.property) == false)
+                    return;
+                var state = subject.property.name;
+                _this.state.push(state);
+            }
+        });
+        this.state = Array.from(new Set(this.state));
+        console.log(JSON.stringify(this.state, null, 2));
+    };
+    AST.prototype.processProps = function () {
+        var _this = this;
+        traverse_1.default(this.tree, {
+            MemberExpression: function (p) {
+                var path = p;
+                var member = path.node;
+                if (Babel.isMemberExpression(member.object) == false)
+                    return;
+                var subject = member.object;
+                // Template.instance(), templateInstance ou this
+                if ((Babel.isThisExpression(subject.object)
+                    || (Babel.isIdentifier(subject.object) && subject.object.name === 'templateInstance')
+                    || (Babel.isCallExpression(subject.object)
+                        && subject.object.callee.object.name === 'Template'
+                        && subject.object.callee.property.name === 'instance')) == false)
+                    return;
+                // .data
+                if ((Babel.isIdentifier(subject.property) && subject.property.name === "data") == false)
+                    return;
+                if (Babel.isIdentifier(member.property) == false)
+                    return;
+                var prop = member.property.name;
+                _this.props.push(prop);
+            }
+        });
+        this.props = Array.from(new Set(this.props));
+        //console.log(JSON.stringify(this.props, null, 2));
+    };
     AST.prototype.processImports = function () {
         var _this = this;
         traverse_1.default(this.tree, {
@@ -50,7 +122,104 @@ var AST = /** @class */ (function () {
                     _this.willUnmount = expr.expression.arguments[0];
             }
         });
-        console.log(JSON.stringify(this.constructr, null, 2));
+        //console.log(JSON.stringify(this.constructr, null, 2));
+    };
+    AST.prototype.processHelpers = function () {
+        this.processTemplateProperty('helpers', this.helpers);
+        //console.log(JSON.stringify(array[0], null, 2));
+    };
+    AST.prototype.processEvents = function () {
+        var fns = [];
+        var selectors = [];
+        this.processTemplateProperty('events', fns, selectors);
+        for (var i = 0; i < selectors.length; i++) {
+            var data = selectors[i].split(' ');
+            var event_1 = data[0];
+            event_1 = 'on' + event_1.replace(/(?:^|\s)\S/g, function (a) { return a.toUpperCase(); });
+            data.splice(0, 1);
+            var selector = data.join(' ');
+            this.events.push({
+                event: event_1,
+                selector: selector,
+                fun: fns[i],
+            });
+        }
+        //console.log(JSON.stringify(this.events[0], null, 2));
+    };
+    AST.prototype.processTemplateProperty = function (prop, array, opts) {
+        var _this = this;
+        if (opts === void 0) { opts = null; }
+        traverse_1.default(this.tree, {
+            ExpressionStatement: function (p) {
+                var path = p;
+                var expr = path.node;
+                if (Babel.isCallExpression(expr.expression) == false)
+                    return;
+                if (Babel.isMemberExpression(expr.expression.callee) == false)
+                    return;
+                if (Babel.isMemberExpression(expr.expression.callee.object) == false)
+                    return;
+                if (Babel.isIdentifier(expr.expression.callee.property) == false)
+                    return;
+                var cycle = expr.expression.callee.property;
+                if (cycle.name !== prop)
+                    return;
+                var object = expr.expression.arguments[0];
+                traverse_1.default(object, {
+                    ObjectProperty: function (p) {
+                        var path = p;
+                        var property = path.node;
+                        if (Babel.isFunction(property.value) == false)
+                            return;
+                        var value = property.value;
+                        var key = null;
+                        if (Babel.isIdentifier(property.key))
+                            key = property.key;
+                        else if (Babel.isStringLiteral(property.key)) {
+                            if (opts != null)
+                                opts.push(property.key.value);
+                            key = Babel.identifier("fun_" + _this.getNodeName());
+                        }
+                        var body;
+                        if (Babel.isBlockStatement(value.body))
+                            body = value.body;
+                        else {
+                            body = Babel.blockStatement([
+                                Babel.returnStatement(value.body)
+                            ]);
+                        }
+                        var fun = Babel.functionDeclaration(key, value.params, body, value.generator, value.async);
+                        array.push(fun);
+                    }
+                }, path.scope, path.state, path.parentPath);
+            }
+        });
+    };
+    AST.prototype.sanitizeFunction = function (path, fun) {
+        // Suppression de templateInstance dans la déclaration
+        fun.params = fun.params.filter(function (e) { return (Babel.isIdentifier(e) == false || e.name !== "templateInstance"); });
+        traverse_1.default(fun, {
+            // Remplacement de templateInstance par this dans le corps
+            Identifier: function (p) {
+                var path = p;
+                var id = path.node;
+                if (id.name === "templateInstance")
+                    path.node = Babel.thisExpression();
+            },
+            // Remplacement de Template.instance() par this dans le corps
+            CallExpression: function (p) {
+                var path = p;
+                var cll = path.node;
+                if (Babel.isMemberExpression(cll.callee)) {
+                    var member = cll.callee;
+                    if (Babel.isIdentifier(member.object) && member.object.name == "Template") {
+                        if (Babel.isIdentifier(member.property) && member.property.name == "instance") {
+                            path.node = Babel.identifier("this");
+                        }
+                    }
+                }
+            }
+        }, path.scope, path.state, path.parentPath);
     };
     AST.prototype.processFunctions = function () {
         var _this = this;
@@ -61,37 +230,12 @@ var AST = /** @class */ (function () {
                 // Si c'est une fonction utilitaire
                 if (Babel.isProgram(path.parent)) {
                     _this.funcs.push(fun);
-                    var parent_1 = p.parent;
-                    parent_1.body = parent_1.body.filter(function (e) { return e != p.node; });
                 }
-                // Suppression de templateInstance dans la déclaration
-                fun.params = fun.params.filter(function (e) { return (Babel.isIdentifier(e) == false || e.name !== "templateInstance"); });
-                traverse_1.default(fun, {
-                    // Remplacement de templateInstance par this dans le corps
-                    Identifier: function (p) {
-                        var path = p;
-                        var id = path.node;
-                        if (id.name === "templateInstance")
-                            id.name = "this";
-                    },
-                    // Remplacement de Template.instance() par this dans le corps
-                    CallExpression: function (p) {
-                        var path = p;
-                        var cll = path.node;
-                        if (Babel.isMemberExpression(cll.callee)) {
-                            var member = cll.callee;
-                            if (Babel.isIdentifier(member.object) && member.object.name == "Template") {
-                                if (Babel.isIdentifier(member.property) && member.property.name == "instance") {
-                                    path.node = Babel.identifier("this");
-                                }
-                            }
-                        }
-                    }
-                }, path.scope, path.state, path.parentPath);
             }
         });
         //console.log(JSON.stringify(this.funcs[0], null, 2));
     };
+    AST.NODE_COUNTER = 0;
     return AST;
 }());
 exports.default = AST;
--- src/AST.ts
+++ src/AST.ts
@@ -3,6 +3,13 @@ import * as Babel from '@babel/types';
 import traverse from "@babel/traverse";
 import { NodePath } from '@babel/traverse';
 
+
+interface EventBind {
+  selector: string, 
+  event: string,
+  fun: Babel.FunctionDeclaration
+}
+
 export default class AST {
   
   tree: Babel.File;
@@ -14,6 +21,14 @@ export default class AST {
   didMount: Babel.FunctionDeclaration | null = null;
   willUnmount: Babel.FunctionDeclaration | null = null;
 
+  helpers: Babel.FunctionDeclaration[] = [];
+  events: EventBind[] = [];
+
+  props: string[] = [];
+  state: string[] = [];
+
+  static NODE_COUNTER = 0;
+
   constructor(code: string) {
     this.tree = Parser.parse(code, {
       sourceType: 'module',
@@ -22,6 +37,86 @@ export default class AST {
     this.processImports();
     this.processFunctions();
     this.processLifeCycle();
+    this.processHelpers();
+    this.processEvents();
+    this.processProps();
+    this.processState();
+  }
+
+  private getNodeName() {
+    AST.NODE_COUNTER++;
+    return AST.NODE_COUNTER.toString();
+  }
+
+  processState() {
+    traverse(this.tree, {
+      AssignmentExpression: (p) => {
+        const path: NodePath<Babel.AssignmentExpression> = p;
+        const expr: Babel.AssignmentExpression = path.node;
+        if(Babel.isMemberExpression(expr.left) == false) return;
+        const subject: Babel.MemberExpression = <any>expr.left;
+        // Template.instance(), templateInstance ou this
+        if(
+          (
+            Babel.isThisExpression(subject.object)
+            || (Babel.isIdentifier(subject.object) && (<Babel.Identifier>subject.object).name === 'templateInstance')
+            || (
+              Babel.isCallExpression(subject.object) 
+              && (<Babel.Identifier>(<Babel.MemberExpression>(<Babel.CallExpression>subject.object).callee).object).name === 'Template'
+              && (<Babel.Identifier>(<Babel.MemberExpression>(<Babel.CallExpression>subject.object).callee).property).name === 'instance'
+            )
+          ) ==  false
+        ) return;
+        // = new ReactiveTruc
+        if(
+          (
+            Babel.isNewExpression(expr.right)
+            && Babel.isIdentifier((<Babel.NewExpression>expr.right).callee) 
+            && (
+              (<Babel.Identifier>(<Babel.NewExpression>expr.right).callee).name === "ReactiveVar"
+              || (<Babel.Identifier>(<Babel.NewExpression>expr.right).callee).name === "ReactiveDict"
+            )
+          ) == false
+        ) return;
+        // nom du state
+        if(Babel.isIdentifier(subject.property) == false) return;
+        const state = (<Babel.Identifier>subject.property).name;
+        this.state.push(state);
+      }
+    });
+    this.state = Array.from(new Set(this.state));
+    console.log(JSON.stringify(this.state, null, 2));
+  }
+
+  processProps() {
+    traverse(this.tree, {
+      MemberExpression: (p) => {
+        const path: NodePath<Babel.MemberExpression> = p;
+        const member: Babel.MemberExpression = path.node;
+        if(Babel.isMemberExpression(member.object) == false) return;
+        const subject : Babel.MemberExpression = <any>member.object;
+
+        // Template.instance(), templateInstance ou this
+        if(
+            (
+              Babel.isThisExpression(subject.object)
+              || (Babel.isIdentifier(subject.object) && (<Babel.Identifier>subject.object).name === 'templateInstance')
+              || (
+                Babel.isCallExpression(subject.object) 
+                && (<Babel.Identifier>(<Babel.MemberExpression>(<Babel.CallExpression>subject.object).callee).object).name === 'Template'
+                && (<Babel.Identifier>(<Babel.MemberExpression>(<Babel.CallExpression>subject.object).callee).property).name === 'instance'
+              )
+            ) ==  false
+        ) return;
+        // .data
+        if((Babel.isIdentifier(subject.property) && (<Babel.Identifier>subject.property).name === "data") == false) return;
+        if(Babel.isIdentifier(member.property) == false) return;
+        const prop = (<Babel.Identifier>member.property).name;
+        this.props.push(prop);
+      }
+    });
+    this.props = Array.from(new Set(this.props));
+    //console.log(JSON.stringify(this.props, null, 2));
   }
 
   processImports() {
@@ -49,7 +144,107 @@ export default class AST {
         else if(cycle.name == "onDestroyed") this.willUnmount = expr.expression.arguments[0];
       }
     });
-    console.log(JSON.stringify(this.constructr, null, 2));
+    //console.log(JSON.stringify(this.constructr, null, 2));
+  }
+
+  processHelpers() {
+    this.processTemplateProperty('helpers', this.helpers);
+    //console.log(JSON.stringify(array[0], null, 2));
+
+  }
+
+  processEvents() {
+    const fns: Babel.FunctionDeclaration[] = [];
+    const selectors: string[] = [];
+    this.processTemplateProperty('events', fns, selectors);
+    for(let i = 0; i < selectors.length; i++) {
+      const data = selectors[i].split(' ');
+      let event = data[0];
+      event = 'on' + event.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+      data.splice(0, 1);
+      let selector = data.join(' ');
+      this.events.push({
+        event,
+        selector,
+        fun: fns[i],
+      })
+    }
+    //console.log(JSON.stringify(this.events[0], null, 2));
+  }
+
+  processTemplateProperty(prop: string, array: Babel.FunctionDeclaration[], opts: string[] | null = null) {
+    traverse(this.tree, {
+      ExpressionStatement: (p) => {
+        const path: NodePath<Babel.ExpressionStatement> = p;
+        let expr: any = path.node;
+        if(Babel.isCallExpression(expr.expression) == false) return;
+        if(Babel.isMemberExpression(expr.expression.callee) == false) return;
+        if(Babel.isMemberExpression(expr.expression.callee.object) == false) return;
+        if(Babel.isIdentifier(expr.expression.callee.property) == false) return;
+        const cycle: Babel.Identifier = expr.expression.callee.property;
+        if(cycle.name !== prop) return;
+        const object: Babel.ObjectExpression = expr.expression.arguments[0];
+        traverse(object, {
+          ObjectProperty: (p) => {
+            const path: NodePath<Babel.ObjectProperty> = p;
+            const property: Babel.ObjectProperty = path.node;
+            if(Babel.isFunction(property.value) == false) return;
+            const value: Babel.Function = <any>property.value;
+
+            let key: Babel.Identifier | null = null;
+            if(Babel.isIdentifier(property.key)) key = <any>property.key;
+            else if(Babel.isStringLiteral(property.key)) {
+              if(opts != null) opts.push(property.key.value);
+              key = Babel.identifier(`fun_${this.getNodeName()}`);
+            }
+
+            let body: Babel.BlockStatement;
+            if(Babel.isBlockStatement(value.body)) body = value.body; 
+            else {
+              body = Babel.blockStatement([
+                Babel.returnStatement(value.body)
+              ]);
+            }
+
+            const fun: Babel.FunctionDeclaration = Babel.functionDeclaration(
+              key,
+              value.params,
+              body,
+              value.generator,
+              value.async
+            );
+            array.push(fun);
+          }
+        }, path.scope, path.state, path.parentPath);
+      }
+    });
+  }
+
+  private sanitizeFunction(path: NodePath<any>, fun: Babel.FunctionDeclaration) {
+    // Suppression de templateInstance dans la déclaration
+    fun.params = fun.params.filter((e: Babel.Node) => (Babel.isIdentifier(e) == false || (<Babel.Identifier>e).name !== "templateInstance"));
+
+    traverse(fun, {
+      // Remplacement de templateInstance par this dans le corps
+      Identifier: (p) => {
+        const path: NodePath<Babel.Identifier> = p;
+        const id: Babel.Identifier = path.node;
+        if(id.name === "templateInstance") path.node = <any>Babel.thisExpression();
+      },
+      // Remplacement de Template.instance() par this dans le corps
+      CallExpression: (p) => {
+        const path: NodePath<Babel.CallExpression> = p;
+        const cll: Babel.CallExpression = path.node;
+        if(Babel.isMemberExpression(cll.callee)) {
+          const member = <Babel.MemberExpression>cll.callee;
+          if(Babel.isIdentifier(member.object) && (<Babel.Identifier>member.object).name == "Template") {
+            if(Babel.isIdentifier(member.property) && (<Babel.Identifier>member.property).name == "instance") {
+              (<Babel.Identifier>(<any>path.node)) = Babel.identifier("this");
+            }
+          }
+        }
+      }
+    }, path.scope, path.state, path.parentPath)
   }
 
   processFunctions() {
@@ -60,35 +255,8 @@ export default class AST {
 
         // Si c'est une fonction utilitaire
         if(Babel.isProgram(path.parent)) {
-          this.funcs.push(fun);  
-          const parent: Babel.Program = <Babel.Program>p.parent;
-          parent.body = parent.body.filter(e => e != p.node);      
+          this.funcs.push(fun);     
         }
-
-        // Suppression de templateInstance dans la déclaration
-        fun.params = fun.params.filter((e: Babel.Node) => (Babel.isIdentifier(e) == false || (<Babel.Identifier>e).name !== "templateInstance"));
-
-        traverse(fun, {
-          // Remplacement de templateInstance par this dans le corps
-          Identifier: (p) => {
-            const path: NodePath<Babel.Identifier> = p;
-            const id: Babel.Identifier = path.node;
-            if(id.name === "templateInstance") id.name = "this";
-          },
-          // Remplacement de Template.instance() par this dans le corps
-          CallExpression: (p) => {
-            const path: NodePath<Babel.CallExpression> = p;
-            const cll: Babel.CallExpression = path.node;
-            if(Babel.isMemberExpression(cll.callee)) {
-              const member = <Babel.MemberExpression>cll.callee;
-              if(Babel.isIdentifier(member.object) && (<Babel.Identifier>member.object).name == "Template") {
-                if(Babel.isIdentifier(member.property) && (<Babel.Identifier>member.property).name == "instance") {
-                  (<Babel.Identifier>(<any>path.node)) = Babel.identifier("this");
-                }
-              }
-            }
-          }
-        }, path.scope, path.state, path.parentPath)
       }
     });
     //console.log(JSON.stringify(this.funcs[0], null, 2));
